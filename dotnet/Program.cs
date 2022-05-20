7e5185bae83133d4073a294d7fa9094d4288419b
tickbh <tickdream125@hotmail.com>
2022-05-20 13:46:28 +0800

    Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -2,18 +2,7 @@
 
 using proto.tunm;
 
-
 Console.WriteLine("Hello, World!");
-List<Object> value = new List<Object>();
-value.Add("11????????????");
-
-Console.WriteLine("obj null type = " + value.GetType().Name);
-
-Dictionary<Object, Object> value1 = new Dictionary<Object, Object>();
-
-Console.WriteLine("obj null type = " + value1.GetType().Name);
-byte[] ok = new byte[1];
-Console.WriteLine("obj null type = " + ok.GetType().Name);
 
 proto.tunm.Test.test_encode_u8();
 proto.tunm.Test.test_encode_u16();
@@ -22,3 +11,5 @@ proto.tunm.Test.test_encode_float();
 proto.tunm.Test.test_encode_str();
 proto.tunm.Test.test_encode_map();
 proto.tunm.Test.test_base_proto();
+
+Console.WriteLine("end test!");
